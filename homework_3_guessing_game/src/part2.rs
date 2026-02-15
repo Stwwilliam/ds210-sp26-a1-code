c23de061d60f735bfd5e806af4f978b06069f7bc
Ronan <rowong@bu.edu>
2026-02-15 02:12:26 -0500

    Forgot to remove smt here

--- homework_3_guessing_game/src/part2.rs
+++ homework_3_guessing_game/src/part2.rs
@@ -6,7 +6,7 @@ pub struct Part2 {}
 // Terrible strategy: ask if the number is min, otherwise return max.
 impl Strategy for Part2 {
     fn guess_the_number(player: &mut Player, min: u32, max: u32) -> u32 {
-        println!("{min} to {max}");
+        // println!("{min} to {max}");  # To see number range per iteration
         let guess = (min + max)/2;
         let result = player.ask_to_compare(guess);
 
