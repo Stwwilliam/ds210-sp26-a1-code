d981d8b59ebb3a3b3b03f8ae5e7f5d91c616d2a3
Ronan <rowong@bu.edu>
2026-02-15 00:36:19 -0500

    Added part3 solution

--- homework_3_guessing_game/src/part3.rs
+++ homework_3_guessing_game/src/part3.rs
@@ -17,7 +17,11 @@ impl PlayerTrait for SimulatedPlayer {
      * The function should return false otherwise.
      */
     fn ask_if_equal(&mut self, guess: u32) -> bool {
-        todo!("you did not provide your part 3 solution yet!")
+        if self.the_number == guess {
+            return true;
+        } else {
+            return false;
+        }
     }
     /**
      * This function compares the guess to the number that the player has in mind.
@@ -27,7 +31,13 @@ impl PlayerTrait for SimulatedPlayer {
      * 1 if the number > guess
      */
     fn ask_to_compare(&mut self, guess: u32) -> i32 {
-        todo!("you did not provide your part 3 solution yet!")
+        if self.the_number == guess {
+            return 0;
+        } else if self.the_number < guess {
+            return -1;
+        } else {
+            return 1;
+        }
     }
 }
 
