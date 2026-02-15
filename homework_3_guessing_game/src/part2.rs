da43a96d1cc10c7a9d5230c69b32771cb6679c95
William Zhang <stwwilliam1@gmail.com>
2026-02-14 23:55:41 -0500

    solution for part2

--- homework_3_guessing_game/src/part2.rs
+++ homework_3_guessing_game/src/part2.rs
@@ -7,6 +7,18 @@ pub struct Part2 {}
 impl Strategy for Part2 {
     fn guess_the_number(player: &mut Player, min: u32, max: u32) -> u32 {
         // YOUR SOLUTION GOES HERE.
-        todo!("No solution provided yet")
-    }
+        let result =player.ask_to_compare(min+(max-min)/2);
+        if result==0{
+            if player.ask_if_equal(min+(max-min)/2) {
+                return min+(max-min)/2;
+            }
+            return min;
+        }
+        else if result>0{
+            Self::guess_the_number(player, min+(max-min)/2, max)
+        }
+        else{
+            Self::guess_the_number(player, min, min+(max-min)/2)
+        }
+        }        
 }
