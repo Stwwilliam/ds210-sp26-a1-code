32e300ff8a56ced1f1ded9a0faa9bb0c5c32b3e0
Ronan <rowong@bu.edu>
2026-02-15 01:27:08 -0500

    Added more to part3 sol and modified part2 sol

--- homework_3_guessing_game/src/part2.rs
+++ homework_3_guessing_game/src/part2.rs
@@ -8,15 +8,14 @@ pub struct Part2 {}
 impl Strategy for Part2 {
     fn guess_the_number(player: &mut Player, min: u32, max: u32) -> u32 {
         let guess = random_range(min..max);
+        let result = player.ask_to_compare(guess);
 
-        if player.ask_to_compare(guess) == 0 {
+        if result == 0 {
             return guess;
-        } else if player.ask_to_compare(guess) == -1 {
-            Self::guess_the_number(player, min, guess);
+        } else if result == -1 {
+            Self::guess_the_number(player, min, guess-1)
         } else {
-            Self::guess_the_number(player, guess, max);
+            Self::guess_the_number(player, guess+1, max)
         }
-
-        return 0;
     }
 }
