bcc7339586e14ec05f19e2bdcb5a142251360a08
Ronan <rowong@bu.edu>
2026-02-15 00:20:59 -0500

    Added part2 solution & moditified part1 sol

--- homework_3_guessing_game/src/part2.rs
+++ homework_3_guessing_game/src/part2.rs
@@ -1,12 +1,22 @@
 use crate::player::{Player, PlayerTrait};
 use crate::strategies::Strategy;
+use rand::random_range;
 
 pub struct Part2 {}
 
 // Terrible strategy: ask if the number is min, otherwise return max.
 impl Strategy for Part2 {
     fn guess_the_number(player: &mut Player, min: u32, max: u32) -> u32 {
-        // YOUR SOLUTION GOES HERE.
-        todo!("No solution provided yet")
+        let guess = random_range(min..max);
+
+        if player.ask_to_compare(guess) == 0 {
+            return guess;
+        } else if player.ask_to_compare(guess) == -1 {
+            Self::guess_the_number(player, min, guess);
+        } else {
+            Self::guess_the_number(player, guess, max);
+        }
+
+        return 0;
     }
 }
