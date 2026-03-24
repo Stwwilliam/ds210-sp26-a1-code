c236cb213c263416c82e1f6a32500e8ac1920697
Ronan <rowong@bu.edu>
2026-03-23 21:33:28 -0400

    Implemented chat_with_user

--- project_2_chatbot/cache_chatbot/src/solution/v5.rs
+++ project_2_chatbot/cache_chatbot/src/solution/v5.rs
@@ -21,64 +21,35 @@ impl ChatbotV5 {
         let filename = &format!("{}.txt", username);
         let cached_chat = self.cache.get_chat(&username);
 
-        match cached_chat {
+        let mut chat_session = match cached_chat {
             None => {
                 println!("chat_with_user: {username} is not in the cache!");
-                // The cache does not have the chat. What should you do?
-                // return String::from("Hello, I am not a bot (yet)!");
-
-                let mut chat_session = self.model
-                    .chat()
-                    .with_system_prompt("The assistant will act like a pirate");
-
-                let session = file_library::load_chat_session_from_file(&filename);
-
-                match session {
+                match file_library::load_chat_session_from_file(&filename) {
                     None => {
-                        
+                        Chat::new(self.model.clone())
+                            .with_system_prompt("The assistant will act like a pirate")
                     },
-                    Some(x) => {
-                        chat_session = chat_session.with_session(x);
+                    Some(session) => {
+                        Chat::new(self.model.clone()).with_session(session)
                     }
                 }
-
-                let output = chat_session.add_message(&message).await;
-
-                let session = chat_session.session().unwrap();
-                file_library::save_chat_session_to_file(filename, &session);
-
-                match output {
-                    Err(why) => {
-                        panic!("{:?}", why);
-                    },
-                    Ok(msg) => {
-                        return msg;
-                    }
-                }
-            }
+            },
             Some(chat_session) => {
                 println!("chat_with_user: {username} is in the cache! Nice!");
-                // The cache has this chat. What should you do?
-                // return String::from("Hello, I am not a bot (yet)!");
-                
-                let output = chat_session.add_message(&message).await;
-
-                let session = chat_session.session().unwrap();
-                file_library::save_chat_session_to_file(filename, &session);
-
-                match output {
-                    Err(why) => {
-                        panic!("{:?}", why);
-                    },
-                    Ok(msg) => {
-                        return msg;
-                    }
-                }
+                chat_session.clone()
             }
-        }
+        };
+
+        let output = chat_session.add_message(message).await;
+
+        self.cache.insert_chat(username.clone(), chat_session.clone());
+
+        let session = chat_session.session().unwrap().clone();
+        file_library::save_chat_session_to_file(&username, &session);
+
+        return output.unwrap();
     }
 
-    // student 1
     pub fn get_history(&mut self, username: String) -> Vec<String> {
         let filename = &format!("{}.txt", username);
         let cached_chat = self.cache.get_chat(&username);
