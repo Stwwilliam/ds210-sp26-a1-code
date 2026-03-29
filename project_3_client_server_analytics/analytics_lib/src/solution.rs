c2a86844644908f80836d37f295120e26a89194b
Ronan <rowong@bu.edu>
2026-03-29 17:11:52 -0400

    Implemented filter_dataset and helper function check_conditions

--- project_3_client_server_analytics/analytics_lib/src/solution.rs
+++ project_3_client_server_analytics/analytics_lib/src/solution.rs
@@ -2,14 +2,52 @@ use std::collections::HashMap;
 use crate::dataset::{ColumnType, Dataset, Value, Row};
 use crate::query::{Aggregation, Condition, Query};
 
+
+// both
 pub fn filter_dataset(dataset: &Dataset, filter: &Condition) -> Dataset {
-    todo!("Implement this!");
+    let mut output = Dataset::new(dataset.columns().clone());
+    
+    for row in dataset.iter() {
+        if check_conditions(dataset, filter, row) {
+            output.add_row(row.clone());
+        }
+    }
+
+    return output;
 }
 
+// helper function that checks the equal condition and returns t/f, then checks the other conditions
+fn check_conditions(dataset: &Dataset, filter: &Condition, row: &Row) -> bool {
+
+    match filter {
+        Condition::Equal(string, value) => {
+
+            let col_index = dataset.column_index(string);
+            
+            if row.get_value(col_index) == value {
+                return true;
+            }
+
+            return false;
+        },
+        Condition::Not(condition1) => {
+            !check_conditions(dataset, condition1, row)
+        },
+        Condition::And(condition1, condition2) => {
+            check_conditions(dataset, condition1, row) && check_conditions(dataset, condition2, row)
+        },
+        Condition::Or(condition1, condition2) => {
+            check_conditions(dataset, condition1, row) || check_conditions(dataset, condition2, row)
+        }
+    }
+}
+
+// student 1
 pub fn group_by_dataset(dataset: Dataset, group_by_column: &String) -> HashMap<Value, Dataset> {
     todo!("Implement this!");
 }
 
+// student 2
 pub fn aggregate_dataset(dataset: HashMap<Value, Dataset>, aggregation: &Aggregation) -> HashMap<Value, Value> {
     todo!("Implement this!");
 }
