88d3d56885d91dabb92a20d9bfd6e034027aa002
William Zhang <stwwilliam1@gmail.com>
2026-03-29 19:43:00 -0400

    finished group by

--- project_3_client_server_analytics/analytics_lib/src/solution.rs
+++ project_3_client_server_analytics/analytics_lib/src/solution.rs
@@ -7,8 +7,25 @@ pub fn filter_dataset(dataset: &Dataset, filter: &Condition) -> Dataset {
 }
 
 pub fn group_by_dataset(dataset: Dataset, group_by_column: &String) -> HashMap<Value, Dataset> {
-    todo!("Implement this!");
-}
+    let col_index = dataset.column_index(group_by_column);
+    let mut map: HashMap<Value, Dataset> = HashMap::new();
+    for row in dataset.iter(){
+        let key = row.get_value(col_index).clone();
+        match map.get_mut(&key){
+            Some(value) => {
+                value.add_row(row.clone());
+            },
+            None => {
+                let mut new_dataset = Dataset::new(dataset.columns().clone());
+                new_dataset.add_row(row.clone());
+                map.insert(key.clone(), new_dataset);
+            }
+        };       
+        }
+        return map
+    }
+
+
 
 pub fn aggregate_dataset(dataset: HashMap<Value, Dataset>, aggregation: &Aggregation) -> HashMap<Value, Value> {
     todo!("Implement this!");
