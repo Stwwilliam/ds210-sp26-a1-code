a6698e19401f2bda202f780278bcfca1e51b66de
Ronan <rowong@bu.edu>
2026-04-16 20:15:11 -0400

    Calibrated time limit to 4000 ms

--- project_4_tic_tac_toe/tic_tac_toe_5x5/src/solution/agent.rs
+++ project_4_tic_tac_toe/tic_tac_toe_5x5/src/solution/agent.rs
@@ -11,6 +11,42 @@ impl Agent for SolutionAgent {
     // where <score> is your estimate for the score of the game
     // and <x>, <y> are the position of the move your solution will make.
     fn solve(board: &mut Board, player: Player, _time_limit: u64) -> (i32, usize, usize) {
-        unimplemented!("Not yet implemented")
-    }
+            // If you want to make a recursive call to this solution, use
+            // `SolutionAgent::solve(...)`
+            if board.game_over(){
+                let score = board.score();
+                return (score, 0, 0)
+            }
+            else{
+                let possibilities = board.moves();
+                if player == Player::X{
+                    let mut win = -2;
+                    let mut position = possibilities[0];
+                    for i in 0..possibilities.len(){
+                        let mut diff_board = board.clone();
+                        diff_board.apply_move(possibilities[i], player);
+                        let (score, _, _) = SolutionAgent::solve(&mut diff_board, Player::O, _time_limit);
+                        if score > win {
+                            win = score;
+                            position = possibilities[i]
+                        }
+                    }    
+                    return (win, position.0, position.1);
+                }
+                else {
+                    let mut win = 2;
+                    let mut position = possibilities[0];
+                    for i in 0..possibilities.len(){
+                        let mut diff_board: Board = board.clone();
+                        diff_board.apply_move(possibilities[i], player);
+                        let (score, _, _) = SolutionAgent::solve(&mut diff_board, Player::X, _time_limit);
+                        if score < win {
+                            win = score;
+                            position = possibilities[i]
+                        }
+                    }
+                    return (win, position.0,position.1);
+                }
+            }
+        }
 }
