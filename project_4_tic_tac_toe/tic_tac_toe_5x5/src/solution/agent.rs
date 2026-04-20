9afe51a0f9daa7f38f112894ed2dad093c1a0cc9
William Zhang <stwwilliam1@gmail.com>
2026-04-20 00:39:39 -0400

    Changed solve function

--- project_4_tic_tac_toe/tic_tac_toe_5x5/src/solution/agent.rs
+++ project_4_tic_tac_toe/tic_tac_toe_5x5/src/solution/agent.rs
@@ -153,42 +153,25 @@ impl Agent for SolutionAgent {
     // where <score> is your estimate for the score of the game
     // and <x>, <y> are the position of the move your solution will make.
     fn solve(board: &mut Board, player: Player, _time_limit: u64) -> (i32, usize, usize) {
-            // If you want to make a recursive call to this solution, use
-            // `SolutionAgent::solve(...)`
-            if board.game_over(){
-                let score = board.score();
-                return (score, 0, 0)
-            }
-            else{
-                let possibilities = board.moves();
-                if player == Player::X{
-                    let mut win = -2;
-                    let mut position = possibilities[0];
-                    for i in 0..possibilities.len(){
-                        let mut diff_board = board.clone();
-                        diff_board.apply_move(possibilities[i], player);
-                        let (score, _, _) = SolutionAgent::solve(&mut diff_board, Player::O, _time_limit);
-                        if score > win {
-                            win = score;
-                            position = possibilities[i]
-                        }
-                    }    
-                    return (win, position.0, position.1);
-                }
-                else {
-                    let mut win = 2;
-                    let mut position = possibilities[0];
-                    for i in 0..possibilities.len(){
-                        let mut diff_board: Board = board.clone();
-                        diff_board.apply_move(possibilities[i], player);
-                        let (score, _, _) = SolutionAgent::solve(&mut diff_board, Player::X, _time_limit);
-                        if score < win {
-                            win = score;
-                            position = possibilities[i]
-                        }
-                    }
-                    return (win, position.0,position.1);
-                }
-            }
-        }
+        let move_count = board.moves().len();
+        let depth_limit: usize;
+
+
+        //if move_count < 10 {
+        //  depth_limit = move_count;
+        //} else {
+        //  depth_limit = 4;
+        //}
+       
+        if move_count > 15 {
+            depth_limit = 4;
+        } else if move_count > 10 {
+            depth_limit = 6;
+        } else {
+            depth_limit = move_count;
+        };
+
+
+        SolutionAgent::minimax(board, player, _time_limit, 0, depth_limit)
+    }
 }
