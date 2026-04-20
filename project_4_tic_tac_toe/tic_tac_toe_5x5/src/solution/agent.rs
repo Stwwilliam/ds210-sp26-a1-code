3881914d678744785c76e2381fc466cc5d8ea685
William Zhang <stwwilliam1@gmail.com>
2026-04-20 00:21:37 -0400

    Minimax Function Implemented

--- project_4_tic_tac_toe/tic_tac_toe_5x5/src/solution/agent.rs
+++ project_4_tic_tac_toe/tic_tac_toe_5x5/src/solution/agent.rs
@@ -5,6 +5,54 @@ use tic_tac_toe_stencil::player::Player;
 // Your solution solution.
 pub struct SolutionAgent {}
 
+impl SolutionAgent {
+    fn minimax(board: &mut Board, player: Player, _time_limit: u64, depth: usize, max_depth: usize) -> (i32, usize, usize) {
+        if board.game_over() {
+            let score = board.score();
+            return (score, 0, 0);
+
+
+        } else if depth == max_depth {
+            let score = SolutionAgent::heruistic(board);
+            return (score, 0, 0);
+
+
+        } else {
+            let possibilities = board.moves();
+            if player == Player::X{
+                let mut win = -2;
+                let mut position = possibilities[0];
+                for i in 0..possibilities.len(){
+                    let mut diff_board = board.clone();
+                    diff_board.apply_move(possibilities[i], player);
+                    let (score, _, _) = SolutionAgent::solve(&mut diff_board, Player::O, _time_limit);
+                    if score > win {
+                        win = score;
+                        position = possibilities[i];
+                    }
+                }    
+                return (win, position.0, position.1);
+            }
+            else {
+                let mut win = 2;
+                let mut position = possibilities[0];
+                for i in 0..possibilities.len() {
+                    let mut diff_board = board.clone();
+                    diff_board.apply_move(possibilities[i], player);
+                    let (score, _, _) = SolutionAgent::solve(&mut diff_board, Player::O, _time_limit);
+                    if score < win {
+                        win = score;
+                        position = possibilities[i];
+                    }
+                }
+                return (win, position.0,position.1);
+            }
+        }
+    }
+}
+
+
+
 // Put your solution here.
 impl Agent for SolutionAgent {
     // Should returns (<score>, <x>, <y>)
